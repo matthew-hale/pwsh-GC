689adaf4c7c8ee236bfbe4e106c71356a9980f60
Matthew Hale <matthew.hale@protonmail.com>
2018-10-06 04:58:12 -0400

    Add files via upload
    Initial upload; transferred from previous repo after name change

---
+++ Centra.psm1
@@ -0,0 +1,165 @@
+Function ConvertFrom-UnixTime ($UnixDate) {
+
+<#
+Converts a given Unix timestamp in milliseconds to a System.DateTime object in UTC
+#>
+
+	Process {
+		[datetime]$Origin = '1970-01-01 00:00:00'
+		$Origin = $Origin.ToUniversalTime()
+		$Converted = $Origin.AddSeconds($UnixDate)
+	}
+	End {
+		Return $Converted * 1000 #Remember: GuardiCore works with epoch times in milliseconds
+	}
+} #End ConvertFrom-UnixTime
+
+Function ConvertTo-UnixTime {
+
+<#
+Converts a given UTC DateTime object to Unix time in milliseconds
+#>
+
+	[cmdletbinding()]
+	Param (
+		
+	)
+	Process {
+		
+	}
+	End {
+		
+	}
+} #End ConvertTo-UnixTime
+
+Function Set-Headers {
+
+<#
+Returns correctly formatted authentication header within a headers object for GET requests given an auth token input;
+contains a Post switch that appends "Content-Type","application/json" to the headers for post requests
+#>
+
+	[cmdletbinding()]
+	Param (
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][String]$Token,
+		[Switch]$Post
+	)
+	Process {
+		$Headers = New-Object 'System.Collections.Generic.Dictionary[String,String]'
+		$Headers.add("Authorization","bearer " + $Token)
+		
+		if ($Post) {
+			$Headers.add("Content-Type","application/json")
+		}
+	}
+	End {
+		Return $Headers
+	}
+} #End Set-Headers
+
+Function Get-APIKey {
+
+<#
+Gets and parses an API key from the specified server using the given credentials
+#>
+
+	[cmdletbinding()]
+	Param (
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][String]$Server
+	)
+	Begin {
+		$Uri = "https://" + $Server + ".cloud.guardicore.com/api/v3.0/authenticate"
+		$Credentials = Get-Credential
+		$Body = '{"username": "' + $Credentials.UserName + '", "password": "' + $Credentials.GetNetworkCredential().Password + '"}'
+		$Headers = New-Object 'System.Collections.Generic.Dictionary[String,String]'
+		$Headers.add("Content-Type","application/json")
+	}
+	Process {
+		$OutRaw = Invoke-WebRequest -UseBasicParsing -Uri $Uri -Method 'Post' -Body $Body -Headers $Headers
+		if ($OutRaw.StatusCode -ne 200) {
+			Write-Host "Web server error:"
+			Write-Host $OutRaw.RawContent
+			Return $false
+		}
+		$OutParsed = $OutRaw.Content | ConvertFrom-JSON
+	}
+	End {
+		Return $OutParsed.access_token
+	}
+} #End Get-APIKey
+
+Function Get-NetworkLog {
+
+<#
+Makes an API call to download a csv file of the network log of a management server
+(as defined by, and via the API key generated from, the Get-APIKey function)
+#>
+
+} #End Get-NetworkLog
+
+Function Format-NetworkLog {
+
+<#
+Takes a GuardiCore Network Log as an array of PSCustomObjects (formatted as imported by Import-Csv)
+and filters it by the arguments, then returns a new array;
+note that time is in UTC in YYYY-MM-DD HH:MM:SS format;
+can be conveniently used right after a Get-NetworkLog call via the pipeline
+#>
+
+	[cmdletbinding()]
+	Param (
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][System.Array]$Logs,
+		[String]$Id,
+		[String]$ConnectionType,
+		[String]$Action,
+		[String]$SourceIP,
+		[String]$SourceAsset,
+		[String]$SourceProcess,
+		[String]$DestinationIP,
+		[String]$DestinationAsset,
+		[String]$DestinationProcess,
+		[Int]$DestinationPort,
+		[Int]$Count,
+		[String]$Time,
+		[Boolean]$Incidents,
+		[String]$PolicyRule,
+		[String]$PolicyRuleset,
+		[Boolean]$ExportingError
+	)
+	Begin {
+		
+	}
+	Process {
+		
+	}
+	End {
+		
+	}
+} #End Format-NetworkLog
+
+Function Get-FlowTotal {
+
+<#
+Each flow has a "count" field that increments whenever an identical flow is recorded;
+to obtain the true total number of flows for a given set of flow objects, we need to increment a separate counter by these "count" fields;
+this function does that
+#>
+
+	[cmdletbinding()]
+	Param (
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][System.Array]$Connections
+	)
+	
+	Begin {
+		$Subtotal = 0
+	}
+	Process {
+		foreach ($Flow in $Connections) {
+			$Subtotal += $Flow.count
+		}
+	}
+	End {
+		$Total = $Subtotal
+		Return $Total
+	}
+} #End Get-FlowTotal
