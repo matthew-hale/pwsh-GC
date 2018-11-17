0227667450096d570aed73127a5ca585eca25125
Matthew Hale <matthew.hale@protonmail.com>
2018-11-17 14:35:04 -0500

    Add files via upload

--- Install-GCAgent.ps1
+++ Install-GCAgent.ps1
@@ -32,5 +32,62 @@ Param (
 
 $InstallScript = "install.bat" #Hardcoded variable, I know, I know
 $Uri = "https://" + $AggregatorIP + "/" + $InstallScript #This is done this way because I may need to use $InstallScript again
+$Output = @() #Empty array to contain return messages from each computer
 
-Return "Test"
+$Targets = Get-Content $Computers
+
+#This is to access my local variables in the remote session
+$ArgumentList = $AggregatorIP,$ManagementPassword,$Computers,$InstallScript,$Uri,$Targets
+
+$RemoteScript = {
+	#Testing connectivity to aggregator before we continue
+	$Connectivity = Test-Connection $args[0] -Quiet
+	#If the aggregator isn't reachable from here, log it and exit the Invoke-Command session
+	If ($Connectivity -ne $true) {
+		$LogEntry = [PSCustomObject]@(
+			Message = "Aggregator unreachable"
+		)
+		Return $LogEntry
+		exit
+	}
+	
+	$InstallPath = $HOME + "\Downloads\" + $args[3]
+	$InstallCommand = "& cmd.exe /c " +  + $InstallPath + " " + $args[1]
+	
+	#These are so that we can reset these values after we mess with them to get the cert working
+	$DefaultSecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol
+	$DefaultCertificatePolicy = [System.Net.ServicePointManager]::CertificatePolicy
+	
+	<#
+	This effectively bypasses the cert error you encounter manually browsing to this address.
+	First a new class is created called "TrustAllCertsPolicy," which contains a method called "CheckValidationResult."
+	This method simply returns true.
+	Then we enable all security protocols, and set our new class to be our CertificatePolicy.
+	It's not really ideal but what can you do :P
+	#>
+	
+#Tabbing breaks this for some reason
+Add-Type @"
+	using System.Net;
+	using System.Security.Cryptography.X509Certificates;
+	public class TrustAllCertsPolicy : ICertificatePolicy {
+		public bool CheckValidationResult(
+			ServicePoint srvPoint, X509Certificate certificate,
+			WebRequest request, int certificateProblem) {
+			return true;
+		}
+	}
+"@
+
+	$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+	[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+	[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+	
+	Invoke-WebRequest -UseBasicParsing -Uri $args[4] -Method "Get"
+} #End RemoteScript
+
+ForEach-Object $Target in $Targets {
+	$LogEntry = Invoke-Command -ComputerName $Target -ScriptBlock $RemoteScript
+	$Output += $LogEntry
+} #End foreach
+
