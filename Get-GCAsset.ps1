ccd696f96a3f8d06b8e9811453f51aea59f5cc3d
Matthew Hale <matthew.hale@protonmail.com>
2018-11-27 13:50:56 -0500

    Add files via upload
    Get-GCAsset: Gets the hostname, if available, of a GuardiCore asset given an IP address.

---
+++ Get-GCAsset.ps1
@@ -0,0 +1,62 @@
+<#
+.SYNOPSIS
+	Gets the hostname, if available, of a GuardiCore asset given an IP address.
+
+.DESCRIPTION
+	This script takes one or more IP addresses, and makes a series of API calls that fetch the hostname of the given IP. Requires Centra.psm1.
+
+.PARAMETER Server
+	[System.String] The name of the GuardiCore management server from which to fetch the information, formatted like "cus-5555".
+
+.PARAMETER IP
+	[System.Array] One or more IP addresses.
+
+.PARAMETER Token
+	[System.String] GuardiCore API token. The script will prompt for credentials if no token is provided.
+
+.INPUTS
+	None. This script has no pipeline input.
+
+.OUTPUTS
+	System.Array
+
+#>
+
+
+
+Param (
+	[Parameter(Mandatory=$true)][System.String]$Server,
+	[Parameter(Mandatory=$true)][System.Array]$IPs,
+	[System.String]$Token
+)
+Begin {
+	#Gets an API token via Get-GCAPIKey if no token was provided
+	If (-Not ($Token)) {
+		$Token = Get-GCAPIKey -Server $Server
+	}
+	
+	$Headers = Set-GCHeaders -Token $Token
+	$Uri = "https://" + $Server + ".cloud.guardicore.com/api/v3.0/assets?search="
+	$Output = @()
+}
+Process {
+	foreach ($IP in $IPs) {
+		$TempUri = $Uri + $IP
+		$Json = Invoke-WebRequest -UseBasicParsing -Headers $Headers -Uri $TempUri
+		$JsonParsed = $Json.Content | ConvertFrom-Json
+		$Name = $JsonParsed.objects.vm_name
+		$LineOut = [PSCustomObject]@{
+			IP = $IP
+			Name = $Name
+		}
+		
+		If (-Not ($LineOut.Name)) {
+			$LineOut.Name = "N/A"
+		}
+		
+		$Output += $LineOut
+	}
+}
+End {
+	$Output
+}
