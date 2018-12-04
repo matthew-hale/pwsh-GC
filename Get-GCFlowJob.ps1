fa915920084dd19f7c87a29a23fce5d3536bb28d
Matthew Hale <matthew.hale@protonmail.com>
2018-12-04 14:47:02 -0500

    Removed debug messages

--- Get-GCFlowJob.ps1
+++ Get-GCFlowJob.ps1
@@ -31,9 +31,7 @@ for ($j = $Start; $j -lt $End+1; $j += $1Hour) { #Start time to end time
 	$FromTime = $Start + ($1Hour * $Counter)
 	$ToTime = $FromTime + $1Hour
 	for ($i = 0;$i -lt 500000; $i += 10000) { #chunks of 10000 for timeouts, maximum of offset = 500000 because of hard limit
-		Write-Host doing hour chunk $j offset $i ...
 		$Flows = Get-GCFlow -Key $Key -Offset $i -Limit 10000 -FromTime $FromTime -ToTime $ToTime
-		Write-Host ... above done
 		if ($Flows.count -lt 1) {
 			break
 		}
