15bb11b5b864685d786fd4c16c684394f99d3e0a
Matthew Hale <matthew.hale@protonmail.com>
2018-12-10 07:25:24 -0500

    Delete Import-GCApiKey.ps1

--- Import-GCApiKey.ps1
+++
@@ -1,9 +0,0 @@
-Param (
-	[Parameter(Mandatory=$true)]$AES, #Path to AES Key
-	[Parameter(Mandatory=$true)]$Key #Path to exported API key
-)
-
-$Import = Import-CSV -Path $Key
-$AES = Get-Content -Path $AES
-$Import.Token = $Import.Token | ConvertTo-SecureString -Key $AES
-$Import
