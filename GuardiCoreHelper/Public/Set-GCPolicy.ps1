9478035e46251033c665f1df8c4888ae54acc75a
Matthew Hale <matthew.hale@protonmail.com>
2018-12-16 08:55:02 -0500

    Bugfix
    Set-GCPolicy was using the wrong method. Error fixed, now works correctly.

--- GuardiCoreHelper/Public/Set-GCPolicy.ps1
+++ GuardiCoreHelper/Public/Set-GCPolicy.ps1
@@ -5,11 +5,11 @@ function Set-GCPolicy {
 	[cmdletbinding()]
 	param(
 		[Parameter(Mandatory=$true)][PSCustomObject]$Key,
-		[Parameter(Mandatory=$false)][PSCustomObject]$Policy #Policy object as returned from GuardiCore, but with updated values
+		[Parameter(Mandatory=$false,ValueFromPipeline=$true)][PSCustomObject]$Policy #Policy object as returned from GuardiCore, but with updated values. Accepts the PS object, not JSON data. This allows you to grab a policy, change it via powershell methods, and pass it into this function to update it.
 	)
 	
 	$Uri = $Key.Uri + "visibility/policy/rules/" + $Policy.id
 	
 	$BodyJson = $Policy | ConvertTo-Json -Depth 99
-	Invoke-RestMethod -Uri $Uri -ContentType "application/json" -Authentication Bearer -Token $Key.Token -Body $BodyJson -Method "POST"
+	Invoke-RestMethod -Uri $Uri -ContentType "application/json" -Authentication Bearer -Token $Key.Token -Body $BodyJson -Method "PUT"
 }
