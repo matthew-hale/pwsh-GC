fc2d4d572892fcaaf77330682d184e05594245d0
Matt Hale <matthew.hale@protonmail.com>
2019-05-27 05:01:34 -0400

    fixed unix time conversion functions

--- Public/ConvertTo-GCUnixTime.ps1
+++ Public/ConvertTo-GCUnixTime.ps1
@@ -1,31 +1,13 @@
-<#
-.SYNOPSIS
-	Converts a given System.DateTime object to a Unix timestamp in milliseconds.
-
-.DESCRIPTION
-	GuardiCore uses Unix time in milliseconds for most timestamps in the API. This funciton, and its sister function ConvertFrom-GCUnixTime, allow conversion to/from timestamps (System.DateTime objects) and Unix timestamps (in milliseconds).
-
-.PARAMETER DateTime
-	[DateTime] The timestamp you wish to convert to Unix time in milliseconds.
-
-.INPUTS
-	[DateTime] $DateTime parameter.
-
-.OUTPUTS
-	[Int64] Unix timestamp in milliseconds.
-
-#>
 function ConvertTo-GCUnixTime {
 
 	[cmdletbinding()]
 	param (
-		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][DateTime]$DateTime
+		[Parameter(Mandatory,ValueFromPipeline)][DateTime]$DateTime
 	)
 	process {
-		$Origin = New-Object DateTime 1970, 1, 1, 0, 0, 0, ([DateTimeKind]::Utc)
-		[Int64]$Converted = ($DateTime.ToUniversalTime()-$Origin).TotalMilliseconds
-	}
-	end {
-		$Converted
+		foreach ( $ThisDateTime in $DateTime ) {
+			$Origin = New-Object DateTime 1970, 1, 1, 0, 0, 0, ([DateTimeKind]::Utc)
+			[Int64]($ThisDateTime.ToUniversalTime()-$Origin).TotalMilliseconds
+		}
 	}
 }
