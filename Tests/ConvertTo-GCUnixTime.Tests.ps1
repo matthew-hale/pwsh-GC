74da9f3d633abd15bd0ad1f006ee41d1e5a07036
Matt Hale <matthew.hale@protonmail.com>
2019-06-18 10:11:24 -0400

    Add pester test for ConvertTo-GCUnixTime

---
+++ Tests/ConvertTo-GCUnixTime.Tests.ps1
@@ -0,0 +1,14 @@
+# Import the function
+. "$PWD/../pwsh-GC/Public/ConvertTo-GCUnixTime.ps1"
+
+Describe ConvertTo-GCunixTime {
+	It "Given a datetime string, it returns a Unix timestamp in milliseconds." {
+
+		# Equivalent to Tuesday, February 12, 2019, 7:33:20 PM
+		$DateTime = Get-Date "02/12/2019 19:33:20" -Format (Get-Culture).DateTimeFormat.UniversalSortableDateTimePattern
+		$Date = ConvertTo-GCUnixTime -DateTime $DateTime
+		$Should = "1550000000000"
+
+		$Date | Should -Be $Should
+	}
+}
