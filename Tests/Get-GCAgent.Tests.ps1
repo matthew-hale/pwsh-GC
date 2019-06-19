2cb53a57b6794fe9e7265e961bc4c766667d714e
Matt Hale <matthew.hale@protonmail.com>
2019-06-19 05:13:33 -0400

    Complete Get-GCAgent.Tests.ps1
    I've also added a testing function that consistently unrolls a hashtable
    into a string. The hashtable doesn't have to be ordered. This is used
    for hashtable equality comparisons, which are otherwise not possible
    with pester's "Should -Be $Stuff".

--- Tests/Get-GCAgent.Tests.ps1
+++ Tests/Get-GCAgent.Tests.ps1
@@ -6,6 +6,9 @@
 . "$PWD/../pwsh-GC/Private/GCApiKey-present.ps1"
 . "$PWD/../pwsh-GC/Private/Remove-EmptyKeys.ps1"
 
+# Import test function
+. "$PWD/functions/unrollhash.ps1"
+
 Describe Get-GCAgent {
 	Context "No input" {
 		Mock pwsh-GC-get-request {
@@ -36,12 +39,12 @@ Describe Get-GCAgent {
 		}
 	
 		It "calls pwsh-GC-get-request with the correct Body" {
-			$Should = @{
+			$ShouldData = @{
 				limit = 20
 				offset = 0
 			}
-			$Should = [string]$Should["limit"] + " " + [string]$Should["offset"]
-			$Test = [string]$Result[1]["limit"] + " " + [string]$Result[1]["offset"]
+			$Should = unrollhash $ShouldData
+			$Test = unrollhash $Result[1]
 			$Test | Should -Be $Should
 		}
 	
@@ -54,7 +57,7 @@ Describe Get-GCAgent {
 		$global:GCApiKey = $Hold
 	}
 
-	Context "Simple input" {
+	Context "Simple input: -Limit 100 -Offset 50 -Search testsearch" {
 		Mock pwsh-GC-get-request {
 			@($Uri,$Body,$ApiKey)
 		}
@@ -83,13 +86,69 @@ Describe Get-GCAgent {
 		}
 
 		It "calls pwsh-GC-get-request with the correct Body" {
-			$Should = @{
+			$ShouldData = @{
 				limit = 100
 				offset = 50
-				search = "testsearch"
+				"gc_filter" = "testsearch"
 			}
-			$Should = [string]$Should["limit"] + " " + [string]$Should["offset"] + " " + [string]$Should["search"]
-			$Test = [string]$Result[1]["limit"] + " " + [string]$Result[1]["offset"] + " " + [string]$Result[1]["search"]
+			$Should = unrollhash $ShouldData
+			$Test = unrollhash $Result[1]
+			$Test | Should -Be $Should
+			$Test | Should -Be $Should
+		}
+
+		It "calls pwsh-GC-get-request with the correct ApiKey" {
+			$Should = $global:GCApiKey.Uri + " " + (New-Object PSCredential "user",$global:GCApiKey.Token).GetNetworkCredential().Password
+			$Test = $Result[2].Uri + " " + (New-Object PSCredential "user",$Result[2].Token).GetNetworkCredential().Password
+			$Test | Should -Be $Should
+		}
+
+		$global:GCApiKey = $Hold
+	}
+
+	Context "Complex input (non-GuardiCore-object params)" {
+		Mock pwsh-GC-get-request {
+			@($Uri,$Body,$ApiKey)
+		}
+	
+		Mock GCApiKey-present {
+			$true
+		}
+
+		if ($global:GCApiKey) {
+			$Hold = $global:GCApiKey
+		} else {
+			$Hold = $null
+		}
+
+		$global:GCApiKey = [PSCustomObject]@{
+			PSTypeName = "GCApiKey"
+			Token = (ConvertTo-SecureString "testtoken" -AsPlainText -Force)
+			Uri = "testuri"
+		}
+
+		$Result = Get-GCAgent -Limit 130 -Offset 500 -OS "Linux" -Status "Online" -Flag @(2..4) -Enforcement "Active" -Deception "Active" -Detection "Active" -Reveal "Active" -Activity "last_month"
+
+		It "calls pwsh-GC-get-request with the correct Uri" {
+			$Should = "/agents"
+			$Result[0] | Should -Be $Should
+		}
+
+		It "calls pwsh-GC-get-request with the correct Body" {
+			$ShouldData = [ordered]@{
+				limit = 130
+				offset = 500
+				os = "Linux"
+				display_status = "Online"
+				status_flags = "2,3,4"
+				module_status_deception = "Active"
+				module_status_detection = "Active"
+				module_status_reveal = "Active"
+				module_status_enforcement = "Active"
+				activity = "last_month"
+			}
+			$Should = unrollhash $ShouldData
+			$Test = unrollhash $Result[1]
 			$Test | Should -Be $Should
 		}
 
