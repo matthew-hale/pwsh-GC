cddde56f2b56eddd2b86e10221194275c2380d17
Matt Hale <matthew.hale@protonmail.com>
2019-06-19 05:52:55 -0400

    Add -raw parameter test

--- Tests/Get-GCAgent.Tests.ps1
+++ Tests/Get-GCAgent.Tests.ps1
@@ -57,6 +57,57 @@ Describe Get-GCAgent {
 		$global:GCApiKey = $Hold
 	}
 
+	Context "Raw switch" {
+		Mock pwsh-GC-get-request {
+			@($Uri,$Body,$ApiKey,$Raw)
+		}
+	
+		Mock GCApiKey-present {
+			$true
+		}
+
+		if ($global:GCApiKey) {
+			$Hold = $global:GCApiKey
+		} else {
+			$Hold = $null
+		}
+
+		$global:GCApiKey = [PSCustomObject]@{
+			PSTypeName = "GCApiKey"
+			Token = (ConvertTo-SecureString "testtoken" -AsPlainText -Force)
+			Uri = "testuri"
+		}
+	
+		$Result = Get-GCAgent -Raw
+
+		It "calls pwsh-GC-get-request with the correct Uri" {
+			$Should = "/agents"
+			$Result[0] | Should -Be $Should
+		}
+	
+		It "calls pwsh-GC-get-request with the correct Body" {
+			$ShouldData = @{
+				limit = 20
+				offset = 0
+			}
+			$Should = unrollhash $ShouldData
+			$Test = unrollhash $Result[1]
+			$Test | Should -Be $Should
+		}
+	
+		It "calls pwsh-GC-get-request with the correct ApiKey" {
+			$Should = $global:GCApiKey.Uri + " " + (New-Object PSCredential "user",$global:GCApiKey.Token).GetNetworkCredential().Password
+			$Test = $Result[2].Uri + " " + (New-Object PSCredential "user",$Result[2].Token).GetNetworkCredential().Password
+			$Test | Should -Be $Should
+		}
+
+		It "calls pwsh-GC-get-request with the Raw switch" {
+			$Result[3] | Should -Be $true
+		}
+
+		$global:GCApiKey = $Hold
+	}
+
 	Context "Simple input: -Limit 100 -Offset 50 -Search testsearch" {
 		Mock pwsh-GC-get-request {
 			@($Uri,$Body,$ApiKey)
@@ -106,7 +157,7 @@ Describe Get-GCAgent {
 		$global:GCApiKey = $Hold
 	}
 
-	Context "Complex input (non-GuardiCore-object params)" {
+	Context "Complex input: all non-GuardiCore object params" {
 		Mock pwsh-GC-get-request {
 			@($Uri,$Body,$ApiKey)
 		}
