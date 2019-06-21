bb3877747cd46f7045186c97bc666b41b3a93561
Matt Hale <matthew.hale@protonmail.com>
2019-06-21 14:28:39 -0400

    Retab

--- tests/ConvertTo-GCUnixTime.Tests.ps1
+++ tests/ConvertTo-GCUnixTime.Tests.ps1
@@ -2,13 +2,13 @@
 . "$PWD/../pwsh-GC/Public/ConvertTo-GCUnixTime.ps1"
 
 Describe ConvertTo-GCunixTime {
-	It "Given a datetime string, it returns a Unix timestamp in milliseconds." {
+    It "Given a datetime string, it returns a Unix timestamp in milliseconds." {
 
-		# Equivalent to Tuesday, February 12, 2019, 7:33:20 PM
-		$DateTime = Get-Date "02/12/2019 19:33:20" -Format (Get-Culture).DateTimeFormat.UniversalSortableDateTimePattern
-		$Date = ConvertTo-GCUnixTime -DateTime $DateTime
-		$Should = "1550000000000"
+        # Equivalent to Tuesday, February 12, 2019, 7:33:20 PM
+        $DateTime = Get-Date "02/12/2019 19:33:20" -Format (Get-Culture).DateTimeFormat.UniversalSortableDateTimePattern
+        $Date = ConvertTo-GCUnixTime -DateTime $DateTime
+        $Should = "1550000000000"
 
-		$Date | Should -Be $Should
-	}
+        $Date | Should -Be $Should
+    }
 }
