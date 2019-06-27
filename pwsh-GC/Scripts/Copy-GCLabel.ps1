867145d88ba0b07631b19fd52c06513adf815156
Matt Hale <matthew.hale@protonmail.com>
2019-06-27 06:48:29 -0400

    Fix manually added assets functionality

--- pwsh-GC/Scripts/Copy-GCLabel.ps1
+++ pwsh-GC/Scripts/Copy-GCLabel.ps1
@@ -9,17 +9,22 @@ begin {
 	New-GCBlankLabel -LabelKey $LabelKey -LabelValue $LabelValue
 }
 process {
-	$Criteria = $Label.criteria | Select-Object -ExcludeProperty "label_id","source"
+	$DynamicCriteria = $Label.dynamic_criteria
 	$AddedAssets = $Label.added_assets
+    $Assets = foreach ($Asset in $AddedAssets) {
+        Get-GCAsset -Search $Asset.name | Where-Object {$_.id -match $Asset._id}
+    }
 }
 end {
 	$NewLabel = Get-GCLabel -FindMatches -LabelKey $LabelKey -LabelValue $LabelValue
-	if ($Criteria) {
-		$NewLabel.criteria += $Criteria
-	}
-	if ($AddedAssets) {
-		$NewLabel.added_assets = $AddedAssets
+	if ($DynamicCriteria) {
+        $NewLabel | Add-Member -MemberType NoteProperty -Name criteria -Value $DynamicCriteria
+        $NewLabel.dynamic_criteria += $DynamicCriteria
 	}
 
 	$NewLabel | Set-GCLabel
+
+	if ($AddedAssets) {
+        New-GCStaticLabel -LabelKey $LabelKey -LabelValue $LabelValue -Asset $Assets
+	}
 }
