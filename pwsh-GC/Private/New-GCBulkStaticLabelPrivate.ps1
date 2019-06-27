0da1a32ce1ace1f37c514a074333634f2366dff8
Matt Hale <matthew.hale@protonmail.com>
2019-06-27 15:00:44 -0400

    Update old function

--- pwsh-GC/Private/New-GCBulkStaticLabelPrivate.ps1
+++ pwsh-GC/Private/New-GCBulkStaticLabelPrivate.ps1
@@ -1,43 +1,27 @@
-<#
-.SYNOPSIS
-	Encapsulates the "POST /visibility/labels/bulk" API call.
-
-.DESCRIPTION
-	Similar to New-GCStaticLabel, except taking an array of label objects instead of creating a single label.
-
-.PARAMETER
-	
-
-.INPUTS
-	None. This function takes no pipeline input.
-
-.OUTPUTS
-	application/json
-
-#>
 function New-GCBulkStaticLabelPrivate {
-	param (
-		[Parameter(ValueFromPipeline)]
-		[System.Object[]]$Labels,
+    param (
+        [Parameter(ValueFromPipeline)]
+        [System.Object[]]$Label,
 
-		[PSTypeName("GCApiKey")]$ApiKey
-	)
-	begin {
-		if ( GCApiKey-present $ApiKey ) {
-			if ( $ApiKey ) {
-				$Key = $ApiKey
-			} else {
-				$Key = $global:GCApiKey
-			} 
-			$Uri = "/visibility/labels/bulk"
-		}
-	}
-	process {
-		$Body = [PSCustomObject]@{
-			"action" = "add"
-			"labels" = $Labels
-		}
+        [PSTypeName("GCApiKey")]$ApiKey
+    )
+    begin {
+        if ( GCApiKey-present $ApiKey ) {
+            if ( $ApiKey ) {
+                $Key = $ApiKey
+            } else {
+                $Key = $global:GCApiKey
+            } 
+            $Uri = "/visibility/labels/bulk"
+        }
+    }
+    process {
+        $Body = [PSCustomObject]@{
+            "action" = "add"
+            "labels" = $Labels
+        }
 
-		pwsh-GC-post-request -Raw -Uri $Uri -Body $Body -ApiKey $Key
-	}
+        pwsh-GC-post-request -Raw -Uri $Uri -Body $Body -ApiKey $Key
+    }
 }
+
--- pwsh-GC/Public/New-GCBlankLabel.ps1
+++ pwsh-GC/Public/New-GCBlankLabel.ps1
@@ -1,36 +1,36 @@
 function New-GCBlankLabel {
-	[cmdletbinding(SupportsShouldProcess)]
+    [cmdletbinding(SupportsShouldProcess)]
 
-	param (
-		[Parameter(Mandatory)]
-		[String]
+    param (
+        [Parameter(Mandatory)]
+        [String]
         $LabelKey,
 
-		[Parameter(Mandatory)]
-		[String]
+        [Parameter(Mandatory)]
+        [String]
         $LabelValue,
 
-		[PSTypeName("GCApiKey")]
+        [PSTypeName("GCApiKey")]
         $ApiKey
-	)
+    )
 
-	if ( GCApiKey-present $ApiKey ) {
-		if ( $ApiKey ) {
-			$Key = $ApiKey
-		} else {
-			$Key = $global:GCApiKey
-		} 
-		$Uri = "/visibility/labels"
-	}
+    if ( GCApiKey-present $ApiKey ) {
+        if ( $ApiKey ) {
+            $Key = $ApiKey
+        } else {
+            $Key = $global:GCApiKey
+        } 
+        $Uri = "/visibility/labels"
+    }
 
-	$Body = [PSCustomObject]@{
-		id = $null
-		key = $LabelKey
-		value = $LabelValue
-		criteria = @()
-	}
+    $Body = [PSCustomObject]@{
+        id = $null
+        key = $LabelKey
+        value = $LabelValue
+        criteria = @()
+    }
 
     if ( $PSCmdlet.ShouldProcess($Body,"pwsh-GC-post-request on $Uri with $Key") ) {
-	    pwsh-GC-post-request -Raw -Uri $Uri -Body $Body -ApiKey $Key
+        pwsh-GC-post-request -Raw -Uri $Uri -Body $Body -ApiKey $Key
     }
 }
