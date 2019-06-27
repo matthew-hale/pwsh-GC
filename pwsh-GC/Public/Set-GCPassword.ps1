23150b366e0344e11061af6453ae5470023cc452
Matt Hale <matthew.hale@protonmail.com>
2019-06-27 15:16:32 -0400

    Update whitespace & formatting

--- pwsh-GC/Public/Set-GCPassword.ps1
+++ pwsh-GC/Public/Set-GCPassword.ps1
@@ -1,44 +1,40 @@
 function Set-GCPassword {
-	[cmdletbinding()]
+    [cmdletbinding()]
 
-	param(
-		# User object as returned from GuardiCore, but with updated values.
-		# Accepts the PS object, not JSON data.
-		# This allows you to grab a user, change it via powershell methods,
-		# and pass it into this function to update it.
-		[Parameter(ValueFromPipeline)]
-		[PSTypeName("GCUser")]$User,
+    param(
+        [Parameter(ValueFromPipeline)]
+        [PSTypeName("GCUser")]$User,
 
-		# There's no way to not use a regular string here.
-		# Besides, it's meant to be a temporary password anyway.
-		[Parameter(Mandatory)]
-		[String]$Password,
+        # There's no way to not use a regular string here.
+        # Besides, it's meant to be a temporary password anyway.
+        [Parameter(Mandatory)]
+        [String]$Password,
 
-		[PSTypeName("GCApiKey")]$ApiKey
-	)
-	
-	begin {
-		if ( GCApiKey-present $ApiKey ) {
-			if ( $ApiKey ) {
-				$Key = $ApiKey
-			} else {
-				$Key = $global:GCApiKey
-			} 
-			$Uri = "/system/user"
-		}
-	}
+        [PSTypeName("GCApiKey")]$ApiKey
+    )
+    
+    begin {
+        if ( GCApiKey-present $ApiKey ) {
+            if ( $ApiKey ) {
+                $Key = $ApiKey
+            } else {
+                $Key = $global:GCApiKey
+            } 
+            $Uri = "/system/user"
+        }
+    }
 
-	process {
-		foreach ( $ThisUser in $User ) {
-			# Serialize/deserialize
-			$RequestUser = $ThisUser | ConvertTo-Json -Depth 2 | ConvertFrom-Json
+    process {
+        foreach ( $ThisUser in $User ) {
+            # Serialize/deserialize
+            $RequestUser = $ThisUser | ConvertTo-Json -Depth 2 | ConvertFrom-Json
 
-			$RequestUser | Add-Member -MemberType NoteProperty -Name "action" -Value "update"
-			$RequestUser | Add-Member -MemberType NoteProperty -Name "password" -Value $Password
-			$RequestUser | Add-Member -MemberType NoteProperty -Name "password_confirm" -Value $Password
-			$RequestBody = $RequestUser | Select-Object -Property action,can_access_passwords,description,email,id,permission_scheme_ids,two_factor_auth_enabled,username,password,password_confirm
+            $RequestUser | Add-Member -MemberType NoteProperty -Name "action" -Value "update"
+            $RequestUser | Add-Member -MemberType NoteProperty -Name "password" -Value $Password
+            $RequestUser | Add-Member -MemberType NoteProperty -Name "password_confirm" -Value $Password
+            $RequestBody = $RequestUser | Select-Object -Property action,can_access_passwords,description,email,id,permission_scheme_ids,two_factor_auth_enabled,username,password,password_confirm
 
-			pwsh-GC-post-request -Raw -Uri $Uri -Body $RequestBody -Method Post -ApiKey $Key
-		}
-	}
+            pwsh-GC-post-request -Raw -Uri $Uri -Body $RequestBody -Method Post -ApiKey $Key
+        }
+    }
 }
--- pwsh-GC/Public/Set-GCPolicy.ps1
+++ pwsh-GC/Public/Set-GCPolicy.ps1
@@ -1,59 +1,57 @@
-# Encapsulates the "PUT visibility/policy/rules/{ruleID}" API call
-
 function Set-GCPolicy {
-	
-	[CmdletBinding()]
-	param (
-		[Parameter(ValueFromPipeline)]
-		[PSTypeName("GCPolicy")]$Policy 
-		# One or more policy objects as returned from GuardiCore, but with updated values. Accepts the PS object, not JSON data. This allows you to grab a policy, change it via powershell methods, and pass it into this function to update it.
-	)
-	begin {
-		if ( GCApiKey-present $ApiKey ) {
-			if ( $ApiKey ) {
-				$Key = $ApiKey
-			} else {
-				$Key = $global:GCApiKey
-			} 
-		}
-	}
-	process {
-		foreach ($ThisPolicy in $Policy) {
-			# Serialize/deserialize data
-			$PCopy = $ThisPolicy | ConvertTo-Json -Depth 99 | ConvertFrom-Json
-			$Uri = "/visibility/policy/rules/" + $PCopy.id
-			
-			# Have to parse the source/destination labels to only contain IDs,
-			# instead of all the other info that they come with from Get-GCLabel
-			# Passing that extra info to the API errors out,
-			# because this api call is just like the one for creating new policy,
-			# and only uses label IDs for the source/destination
-			
-			if ($PCopy.source.labels) {
-				$OrCount = $PCopy.source.labels.or_labels.count
-				for ($i = 0; $i -lt $OrCount; $i++) {
-					$AndCount = $PCopy.source.labels.or_labels[$i].and_labels.count
-					for ($j = 0; $j -lt $AndCount; $j++) {
-						$temp = $PCopy.source.labels.or_labels[$i].and_labels[$j].id
-						$PCopy.source.labels.or_labels[$i].and_labels[$j] = $temp
-					}
-				}
-			}
-			
-			if ($PCopy.destination.labels) {
-				$OrCount = $PCopy.destination.labels.or_labels.count
-				for ($i = 0; $i -lt $OrCount; $i++) {
-					$AndCount = $PCopy.destination.labels.or_labels[$i].and_labels.count
-					for ($j = 0; $j -lt $AndCount; $j++) {
-						$temp = $PCopy.destination.labels.or_labels[$i].and_labels[$j].id
-						$PCopy.destination.labels.or_labels[$i].and_labels[$j] = $temp
-					}
-				}
-			}
-			
-			$RequestBody = $PCopy
+    
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [PSTypeName("GCPolicy")]
+        $Policy 
+    )
+    begin {
+        if ( GCApiKey-present $ApiKey ) {
+            if ( $ApiKey ) {
+                $Key = $ApiKey
+            } else {
+                $Key = $global:GCApiKey
+            } 
+        }
+    }
+    process {
+        foreach ($ThisPolicy in $Policy) {
+            # Serialize/deserialize data
+            $PCopy = $ThisPolicy | ConvertTo-Json -Depth 99 | ConvertFrom-Json
+            $Uri = "/visibility/policy/rules/" + $PCopy.id
+            
+            # Have to parse the source/destination labels to only contain IDs,
+            # instead of all the other info that they come with from Get-GCLabel
+            # Passing that extra info to the API errors out,
+            # because this api call is just like the one for creating new policy,
+            # and only uses label IDs for the source/destination
+            
+            if ( $PCopy.source.labels ) {
+                $OrCount = $PCopy.source.labels.or_labels.count
+                for ($i = 0; $i -lt $OrCount; $i++) {
+                    $AndCount = $PCopy.source.labels.or_labels[$i].and_labels.count
+                    for ($j = 0; $j -lt $AndCount; $j++) {
+                        $temp = $PCopy.source.labels.or_labels[$i].and_labels[$j].id
+                        $PCopy.source.labels.or_labels[$i].and_labels[$j] = $temp
+                    }
+                }
+            }
+            
+            if ( $PCopy.destination.labels ) {
+                $OrCount = $PCopy.destination.labels.or_labels.count
+                for ($i = 0; $i -lt $OrCount; $i++) {
+                    $AndCount = $PCopy.destination.labels.or_labels[$i].and_labels.count
+                    for ($j = 0; $j -lt $AndCount; $j++) {
+                        $temp = $PCopy.destination.labels.or_labels[$i].and_labels[$j].id
+                        $PCopy.destination.labels.or_labels[$i].and_labels[$j] = $temp
+                    }
+                }
+            }
+            
+            $RequestBody = $PCopy
 
-			pwsh-GC-post-request -Raw -Uri $Uri -Body $RequestBody -Method Put -ApiKey $Key
-		}
-	}
+            pwsh-GC-post-request -Raw -Uri $Uri -Body $RequestBody -Method Put -ApiKey $Key
+        }
+    }
 }
